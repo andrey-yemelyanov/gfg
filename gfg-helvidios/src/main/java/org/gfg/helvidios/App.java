0b8cf2a5d26de815ff1ee6cc1340d4cdfee6593c
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2019-03-30 10:50:52 +0100

    initial commit

---
+++ gfg-helvidios/src/main/java/org/gfg/helvidios/App.java
@@ -0,0 +1,13 @@
+package org.gfg.helvidios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
