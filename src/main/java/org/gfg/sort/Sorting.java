b1b4945a900fcf467bd72452e415b9da95f225d0
Andrey Yemelyanov <andrey.yemelyanov.2@consultant.volvo.com>
2019-04-02 11:20:41 +0200

    merge sort

--- src/main/java/org/gfg/sort/Sorting.java
+++ src/main/java/org/gfg/sort/Sorting.java
@@ -3,7 +3,8 @@ package org.gfg.sort;
 public class Sorting{
 
     /**
-     * <p>Sorts the input array using an insertion sort algorithm in O(n^2) time, where n is the length of the array.</p>
+     * <p>Sorts the input array in place using an insertion sort algorithm in O(n^2) time, where n is the length of the array.
+     * Sorting is in non-decreasing order according to natural ordering.</p>
      * @param <T> type of array elements, which must implement {@link Comparable}
      * @param array input array
      */
@@ -43,5 +44,40 @@ public class Sorting{
             if(array[i].compareTo(array[i - 1]) < 0) return false;
         }
         return true;
+    }
+
+    /**
+     * <p>Sorts an input array using a merge sort algorithm in O(nlogn) time.
+     * Sorting is in non-decreasing order according to natural ordering.</p>
+     * @param <T> type of input array elements, must be {@link Comparable}
+     * @param array input array
+     */
+    public static <T extends Comparable<T>> void mergeSort(T[] array){
+        mergeSort(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(T[] array, int i, int j){
+        if(i >= j) return;
+        int mid = i + (j - i) / 2;
+        mergeSort(array, i, mid);
+        mergeSort(array, mid + 1, j);
+        merge(array, i, mid, j);
+    }
+
+    private static <T extends Comparable<T>> void merge(T[] arr, int i, int mid, int j){
+        Object[] leftArr = new Object[mid - i + 1];
+        Object[] rightArr = new Object[j - mid];
+        for(int k = 0; k < leftArr.length; k++) leftArr[k] = arr[i + k];
+        for(int k = 0; k < rightArr.length; k++) rightArr[k] = arr[mid + k + 1];
+        int l = 0; int r = 0; int k = i;
+        while(l < leftArr.length && r < rightArr.length){
+            if(((T) leftArr[l]).compareTo((T) rightArr[r]) <= 0){
+                arr[k++] = (T) leftArr[l++];
+            }else{
+                arr[k++] = (T) rightArr[r++];
+            }
+        }
+        while(l < leftArr.length) arr[k++] = (T) leftArr[l++];
+        while(r < rightArr.length) arr[k++] = (T) rightArr[r++];
     }
 }
--- src/test/java/org/gfg/sort/SortingTest.java
+++ src/test/java/org/gfg/sort/SortingTest.java
@@ -64,4 +64,77 @@ public class SortingTest{
         insertionSort(array);
         assertThat(isSorted(array), is(true));
     }
+
+    @Test
+    public void mergeSortEmptyArray(){
+        Integer[] array = new Integer[]{};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{}));
+    }
+
+    @Test
+    public void mergeSortArrayWithOneElement(){
+        Integer[] array = new Integer[]{1};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1}));
+    }
+
+    @Test
+    public void mergeSortArrayWithTwoElements(){
+        Integer[] array = new Integer[]{1, 2};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1, 2}));
+
+        array = new Integer[]{2, 1};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1, 2}));
+    }
+
+    @Test
+    public void mergeSortArrayWithThreeElements(){
+        Integer[] array = new Integer[]{3, 1, 2};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1, 2, 3}));
+
+        array = new Integer[]{2, 1, 2};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1, 2, 2}));
+
+        array = new Integer[]{2, 2, 1};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1, 2, 2}));
+    }
+
+    @Test
+    public void mergeSortAlreadySortedArray(){
+        Integer[] array = new Integer[]{1, 2, 3, 4, 5, 6};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1, 2, 3, 4, 5 ,6}));
+    }
+
+    @Test
+    public void mergeSortReverseSortedArray(){
+        Integer[] array = new Integer[]{6, 5, 4, 3, 2, 1};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1, 2, 3, 4, 5 ,6}));
+    }
+
+    @Test
+    public void mergeSortArrayWithDuplicates(){
+        Integer[] array = new Integer[]{6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5};
+        mergeSort(array);
+        assertThat(array, is(new Integer[]{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6}));
+    }
+
+    @Test
+    public void mergeSortOnRandomArray(){
+        Integer[] array = new Random().ints(5, 0, 100)
+                                      .boxed()
+                                      .collect(Collectors.toList())
+                                      .toArray(new Integer[0]);
+        mergeSort(array);
+        assertThat(String.format(
+            "Array must be sorted but was %s\n", Arrays.toString(array)), 
+            isSorted(array), is(true));
+    }
 }
