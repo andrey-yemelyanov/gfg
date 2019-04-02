d2ba5bc2cc2d291e21ab27628788299d43baa6f9
Andrey Yemelyanov <andrey.yemelyanov.2@consultant.volvo.com>
2019-04-02 15:07:12 +0200

    ceil and floor

--- src/main/java/org/gfg/search/SearchAlgorithms.java
+++ src/main/java/org/gfg/search/SearchAlgorithms.java
@@ -47,4 +47,57 @@ public class SearchAlgorithms{
         }
         return 0;
     }
+
+    /**
+     * Given a sorted array and a value x, returns the smallest element
+     * in the array greater than or equal to x.
+     * @param <T> type of {@link Comparable} elements
+     * @param array input array in sorted order
+     * @param x value whose ceiling is to be computed
+     * @return index of the ceiling element or -1 if none exists
+     */
+    public static <T extends Comparable<T>> int ceil(T[] array, T x){
+        int from = 0; int to = array.length - 1;
+        while(from <= to){
+            int mid = from + (to - from) / 2;
+            if(array[mid].compareTo(x) == 0) return mid;
+            if((mid == 0 && array[mid].compareTo(x) > 0) || 
+               (mid > 0 && array[mid - 1].compareTo(x) < 0 && array[mid].compareTo(x) > 0)) return mid;
+            if(array[mid].compareTo(x) > 0) to = mid - 1;
+            else from = mid + 1;
+        }
+        return -1;
+    }
+
+    /**
+     * Given a sorted array and a value x, returns the greatest element
+     * in the array less than or equal to x.
+     * @param <T> type of {@link Comparable} elements
+     * @param array input array in sorted order
+     * @param x value whose floor is to be computed
+     * @return index of the floor element or -1 if none exists
+     */
+    public static <T extends Comparable<T>> int floor(T[] array, T x){
+        int from = 0; int to = array.length - 1;
+        while(from <= to){
+            int mid = from + (to - from) / 2;
+            if(array[mid].compareTo(x) == 0) return mid;
+            if((mid == array.length - 1 && array[mid].compareTo(x) < 0) || 
+               (mid < array.length - 1 && array[mid + 1].compareTo(x) > 0 && array[mid].compareTo(x) < 0)) return mid;
+            if(array[mid].compareTo(x) > 0) to = mid - 1;
+            else from = mid + 1;
+        }
+        return -1;
+    }
+
+    /**
+     * Returns a median of two sorted arrays of the same size.
+     * @param <T>
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static <T extends Comparable<T>> T median(T[] arr1, T[] arr2){
+
+    }
 }
--- src/test/java/org/gfg/search/SearchTest.java
+++ src/test/java/org/gfg/search/SearchTest.java
@@ -55,4 +55,68 @@ public class SearchTest{
     public void findPeakInArrayWithAllElementsEqual(){
     	assertThat(findPeak(new int[] {1, 1, 1, 1}), is(1));
     }
+
+    @Test
+    public void ceilEmptyArray(){
+        Integer[] array = new Integer[]{};
+        assertThat(ceil(array, -2), is(-1));
+    }
+
+    @Test
+    public void ceilArrayWithOneElement(){
+        Integer[] array = new Integer[]{5};
+        assertThat(ceil(array, -2), is(0));
+        assertThat(ceil(array, 4), is(0));
+        assertThat(ceil(array, 5), is(0));
+        assertThat(ceil(array, 6), is(-1));
+    }
+
+    @Test
+    public void ceilXIsLessThanAllElements(){
+        Integer[] array = new Integer[]{2,5,7,8,8,9,11,19};
+        assertThat(ceil(array, -2), is(0));
+    }
+
+    @Test
+    public void ceilXIsGreaterThanAllElements(){
+        Integer[] array = new Integer[]{2,5,7,8,8,10,11,19};
+        assertThat(ceil(array, 20), is(-1));
+    }
+
+    @Test
+    public void ceilXExistsInArray(){
+        Integer[] array = new Integer[]{2,5,7,8,8,10,11,19};
+        assertThat(ceil(array, 10), is(5));
+        assertThat(ceil(array, 19), is(7));
+        assertThat(ceil(array, 2), is(0));
+    }
+
+    @Test
+    public void ceilXDoesNotExistInArray(){
+        Integer[] array = new Integer[]{2,5,7,8,8,10,11,19};
+        assertThat(ceil(array, 3), is(1));
+        assertThat(ceil(array, 9), is(5));
+        assertThat(ceil(array, 15), is(7));
+        assertThat(ceil(array, 25), is(-1));
+        assertThat(ceil(array, 1), is(0));
+    }
+
+    @Test
+    public void ceilAllArrayElementsAreSame(){
+        Integer[] array = new Integer[]{2,2,2,2,2,2};
+        assertThat(ceil(array, 3), is(-1));
+        assertThat(ceil(array, 2), is(2));
+        assertThat(ceil(array, 0), is(0));
+    }
+
+    @Test
+    public void floorTest(){
+        Integer[] array = new Integer[]{1, 2, 8, 10, 10, 12, 19};
+        assertThat(floor(array, 3), is(1));
+        assertThat(floor(array, 0), is(-1));
+        assertThat(floor(array, 7), is(1));
+        assertThat(floor(array, 8), is(2));
+        assertThat(floor(array, 30), is(6));
+        assertThat(floor(array, 15), is(5));
+    }
 }
